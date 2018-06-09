4700617d7799c8c1c383c19300081825d9376dfb
Miguel <miguel@antra.io>
2018-06-09 12:30:51 -0500

    asdasda

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,8 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "192.241.208.62:8080",
-  "192.241.217.41:8080",
+  //"192.241.208.62:8080",
+  //"192.241.217.41:8080",
 };
 
 struct CheckpointData {
